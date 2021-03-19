3a2ab64f93f17d3d3317a8b53f8c6387c96d01e5
Jon Penwood <jonp@polarity.io>
2021-03-18 21:58:01 -0500

    Fixed Observables bug where no results were showing up for IPs

--- helpers/getServiceNowObjectType.js
+++ helpers/getServiceNowObjectType.js
@@ -1,8 +1,8 @@
 function getServiceNowObjectType(entityObj) {
-  if (entityObj.types.indexOf('custom.incident') > -1) {
-    return 'incident';
-  } else if (entityObj.types.indexOf('custom.task') > -1) {
+  if (entityObj.types.indexOf('custom.task') > -1) {
     return 'task';
+  } else {
+    return 'incident';
   }
 }
 
